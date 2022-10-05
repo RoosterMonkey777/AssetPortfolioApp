ccab0726efd6dd2caf074c6ce87b2e9a98d2ca75
Zahaak Khan <zuk101777@gmail.com>
2022-10-05 14:49:23 -0400

    Initial Commit

---
+++ StockTradingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StockTradingApp
+//
+//  Created by Zahaak Khan on 2022-10-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StockTradingApp/StockTradingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTradingAppApp.swift
+//  StockTradingApp
+//
+//  Created by Zahaak Khan on 2022-10-05.
+//
+
+import SwiftUI
+
+@main
+struct StockTradingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
