cf347dc4eeff69f00d462b313a752cca60c921c2
Zahaak Khan <zuk101777@gmail.com>
2022-11-20 13:46:59 -0500

    added some login views and created authentication view

---
+++ StockTradingApp/HomePageView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageView.swift
+//  StockTradingApp
+//
+//  Created by Zahaak Khan on 2022-11-20.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
--- StockTradingApp/StockTradingApp.swift
+++ StockTradingApp/StockTradingApp.swift
@@ -1,16 +1,17 @@
-//
-//  StockTradingApp.swift
-//  StockTradingApp
-//
-//  Created by Zahaak Khan on 2022-10-05.
-//
+/*
+    App main
+ */
 
 import SwiftUI
+import Firebase
 import FirebaseCore
+import FirebaseAuth
 
+// configure firebase
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        Auth.auth().useEmulator(withHost: "localhost", port: 9099) //firebase auth emulator suite
         return true
     }
 }
@@ -23,7 +24,28 @@ struct StockTradingApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                // ContentView()
+                AuthenticatedView {
+                    Image(systemName: "number.circle.fill")
+                        .resizable()
+                        .frame(width: 100 , height: 100)
+                        .foregroundColor(Color(.systemPink))
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .clipped()
+                        .padding(4)
+                        .overlay(Circle().stroke(Color.black, lineWidth: 2))
+                    Text("Welcome to My Portfolio")
+                        .font(.title)
+                    Text("Please log in to use the app!")
+                } content: {
+                    HomePageView()
+                    Spacer()
+                }
+            }
         }
     }
 }
+
+
