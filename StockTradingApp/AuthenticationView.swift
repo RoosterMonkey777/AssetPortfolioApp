c86fde39a07e702625a63f54d780d9c4ec61f36a
Zahaak Khan <zuk101777@gmail.com>
2022-11-20 14:13:44 -0500

    Created the authentication models and views. seems like it is working for now, able to create and login with a user account as well as signout and delete

---
+++ StockTradingApp/AuthenticationView.swift
@@ -0,0 +1,32 @@
+//
+//  AuthenticationView.swift
+//  StockTradingApp
+//
+//  Created by Zahaak Khan on 2022-11-20.
+//
+import SwiftUI
+import Combine
+
+struct AuthenticationView: View {
+  @EnvironmentObject var viewModel: AuthenticationViewModel
+
+  var body: some View {
+    VStack {
+      switch viewModel.flow {
+      case .login:
+        LoginView()
+          .environmentObject(viewModel)
+      case .signUp:
+        SignupView()
+          .environmentObject(viewModel)
+      }
+    }
+  }
+}
+
+struct AuthenticationView_Previews: PreviewProvider {
+  static var previews: some View {
+    AuthenticationView()
+      .environmentObject(AuthenticationViewModel())
+  }
+}
