7908e9a50d9ccc8bb1c58096b855df07658892cc
Zahaak Khan <zuk101777@gmail.com>
2022-11-25 12:05:10 -0500

    added extension method for colours

---
+++ StockTradingApp/Extensions/ColorExtension.swift
@@ -0,0 +1,16 @@
+// used to import our custom colors into the app
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let primary = Color("AccentColor")
+    let secondary = Color("Secondary")
+    let background = Color("Background")
+    let bear = Color("Red") // want special red for when price % < 0
+    let bull = Color("Green") // want special red for when price % > 0
+}
