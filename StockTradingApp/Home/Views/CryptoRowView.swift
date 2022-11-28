4c341cb83f4cc61ca40b69b5183311fcd48f89fb
Zahaak Khan <zuk101777@gmail.com>
2022-11-28 18:58:16 -0500

    added  a rowview for crypto

--- StockTradingApp/Home/Views/CryptoRowView.swift
+++ StockTradingApp/Home/Views/CryptoRowView.swift
@@ -1,10 +1,3 @@
-//
-//  CryptoModelView.swift
-//  StockTradingApp
-//
-//  Created by Zahaak Khan on 2022-11-28.
-//
-
 import SwiftUI
 
 struct CryptoRowView: View {
@@ -14,48 +7,75 @@ struct CryptoRowView: View {
     
     var body: some View {
         
-        HStack(spacing: 0){
+        ZStack{
             
-            // left column
-            Text("\(crypto.getRank)")
-                .font(.caption)
-                .foregroundColor(Color.theme.secondary)
-                .frame(minWidth: 30)
+            // background container
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(Color.theme.background)
+                .frame(maxWidth: .infinity, maxHeight: 200)
+                .shadow(color:Color.theme.alien,radius: 10)
+                
+                
             
-            Circle()
-                .frame(width: 30, height: 30)
-            
-            Text(crypto.symbol.uppercased())
-                .font(.headline)
-                .padding(.leading, 6)
-                .foregroundColor(Color.theme.primary)
-            
-            Spacer()
-            
-            // middle column if applicable
-            if showHoldingsColumn {
-                VStack(alignment: .trailing){
-                    Text(crypto.currentCoinHoldingsValue.formatAssetPriceToTwo())
-                        .bold()
-                    Text((crypto.coinHoldings ?? 0).formatAssetPriceToSix())
+            HStack(spacing: 0){
+                
+                // left column
+                VStack(alignment: .center, spacing: 15){
+                    HStack (alignment: .top){
+                        Text("\(crypto.getRank)")
+                            .font(.headline)
+                            .foregroundColor(Color.theme.secondary)
+                            .frame(minWidth: 30)
+                        Text(crypto.symbol.uppercased())
+                            .font(.headline)
+                            .padding(.leading, 6)
+                            .foregroundColor(Color.theme.primary)
+                    }
+                    
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .padding(.horizontal)
+                    
+                    Text(crypto.name.uppercased())
+                        .font(.headline)
+                        .foregroundColor(Color.theme.alien)
                 }
-                .foregroundColor(Color.theme.primary)
-            }
+                
             
-            // riight column
-            VStack(alignment: .trailing){
-                Text("\(crypto.currentPrice.formatAssetPriceToSix())")
-                    .bold()
+                Spacer()
+                
+                // middle column if applicable
+                if showHoldingsColumn {
+                    VStack(alignment: .trailing){
+                        Text(crypto.currentCoinHoldingsValue.formatAssetPriceToTwo())
+                            .bold()
+                        Text((crypto.coinHoldings ?? 0).formatAssetPriceToSix())
+                        
+        
+                    }
                     .foregroundColor(Color.theme.primary)
-                Text("\(crypto.priceChangePercentage24H?.formatToPercentString() ?? "") %")
-                    .foregroundColor(
-                        (crypto.priceChangePercentage24H ?? 0) <= 0 ?
-                        Color.theme.bear :
-                        Color.theme.bull
-                    )
-            }.frame(width: UIScreen.main.bounds.width / 3, alignment: .trailing) //1/3 of the screen
+                    
+                    
+                }
+                
+                // riight column
+                VStack(alignment: .trailing){
+                    Text("\(crypto.currentPrice.formatAssetPriceToSix())")
+                        .bold()
+                        .foregroundColor(Color.theme.primary)
+                    Text("\(crypto.priceChangePercentage24H?.formatToPercentString() ?? "") %")
+                        .foregroundColor(
+                            (crypto.priceChangePercentage24H ?? 0) <= 0 ?
+                            Color.theme.bear :
+                            Color.theme.bull
+                        )
+                }.frame(width: UIScreen.main.bounds.width / 3, alignment: .trailing) //1/3 of the screen
+            }
+            .font(.subheadline)
+            .padding()
+            
         }
-        .font(.subheadline)
+        .padding()
     }
 }
 
